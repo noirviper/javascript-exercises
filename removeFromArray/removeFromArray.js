c5878444674a672a8aa9b9722632f5bc1159099b
Ryan Johnson <christopher.r.johnson2@gmail.com>
2019-09-01 11:00:09 -0700

    removeFromArray tests passed

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
 
+  var temp = [];
+  for (var i = 1; i < arguments.length; i++) {
+      temp.push(arguments[i]);
+      arr = arguments[0].filter(function(value) {
+          return ( value !== temp[i - 1]) ;
+      });
+  }
+  return arr;
+    
 }
 
 module.exports = removeFromArray
