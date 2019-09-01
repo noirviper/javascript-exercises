dd1e6ab299938f8d8d97320a36fcda72299e3883
Ryan Johnson <christopher.r.johnson2@gmail.com>
2019-08-31 23:10:37 -0700

    passed all tests for repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, repeatNum) {
 
+    let stringContainer = "";
+    
+    if (repeatNum <= -1) {
+        stringContainer = "ERROR";
+        return stringContainer;
+        
+    } else if (repeatNum === 0) {
+        return stringContainer;
+        
+    } else {
+        for (i = 0; i < repeatNum; i++) {
+            stringContainer += string;
+        }
+        return stringContainer;    
+    }
+    
+
+    
 }
 
 module.exports = repeatString
