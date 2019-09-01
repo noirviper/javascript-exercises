768a6a800ae1ecfe49e6712cd3fe69604fd279c1
Ryan Johnson <christopher.r.johnson2@gmail.com>
2019-09-01 11:39:01 -0700

    leapYear tests passed

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(arr) {
+    if(arr % 4 === 0) {
+        if (arr % 100 === 0 && arr % 400 === 0) {
+            return true;
+        } else if (arr % 100 === 0) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 }
 
 module.exports = leapYears
