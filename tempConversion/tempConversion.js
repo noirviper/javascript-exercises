218de22abfece32da1429ad157ee6e2fa029dff2
Ryan Johnson <christopher.r.johnson2@gmail.com>
2019-09-01 11:48:32 -0700

    tempConversion tests passed

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(arr) {
+var temp = 5/9*(arr-32);
 
+
+return Math.round(temp*10)/10;
 }
 
-const ctof = function() {
+const ctof = function(arr) {
+  var temp = 9/5*(arr)+32;
 
+
+  return Math.round(temp*10)/10;
 }
 
 module.exports = {
